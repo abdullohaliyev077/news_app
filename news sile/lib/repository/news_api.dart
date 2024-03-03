587122d80046a2d10d6b92951997d231494f94c9
abdullohaliyev077 <githubacount0.com>
2024-03-03 20:44:56 +0500

    yaratildi

---
+++ news sile/lib/repository/news_api.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import '/model/article_model.dart';
+import '/repository/abstract_class_ropository.dart';
+import 'package:http/http.dart' as http;
+import '../model/articles_list_model.dart';
+
+class NewsApi extends ClassRepository {
+  // put the API key here
+  var keyApi = "98b30ab41c8d47a78bd66a3e037a8784";
+
+  @override
+  Future<List<ArticleModel>> getAllNews() async {
+    try {
+      var url =
+          ("https://newsapi.org/v2/top-headlines?country=us&apiKey=$keyApi");
+      http.Response response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        String data = response.body;
+        var jsonData = jsonDecode(data);
+        ArticlesListModel articles = ArticlesListModel.fromMap(jsonData);
+        List<ArticleModel> articlesListModel =
+            articles.articles!.map((e) => ArticleModel.fromMap(e)).toList();
+        return articlesListModel;
+      } else {
+        // here shacked number status code
+        print("status code#Amer# = ${response.statusCode}");
+      }
+    } catch (e) {
+      print(e);
+    }
+    throw Exception("here Amer Exception code staus");
+  }
+
+  @override
+  Future<List<ArticleModel>> getCategory(String category) async {
+    try {
+      var url =
+          ("https://newsapi.org/v2/top-headlines?country=us&category=$category&apiKey=$keyApi");
+      http.Response response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        String data = response.body;
+        var jsonData = jsonDecode(data);
+        ArticlesListModel articles = ArticlesListModel.fromMap(jsonData);
+        List<ArticleModel> articlesListModel =
+            articles.articles!.map((e) => ArticleModel.fromMap(e)).toList();
+        return articlesListModel;
+      } else {
+        // here shacked number status code
+        // ignore: avoid_print
+        print("status code#Amer# = ${response.statusCode}");
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+    throw Exception("here Amer Exception code status");
+  }
+}
+ 
